426fdf15cafac0df87a63d8c5132d6e97cb42f82
Cheng Vue <csalvue@gmail.com>
2019-04-30 19:27:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 // Code your solution here!
 
+function printString(myString) {
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    myString = myString.substring(1, myString.length);
+    printString(myString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(myString) {
+  if (myString === '') {
+    return ''
+  } else {
+    return reverseString(myString.substring(1)) + myString[0]
+  }
+}
+
+function isPalindrome(str) {
+  if (str.length === 0 || str.length === 1) {
+    return true
+  }
+
+  if (str[0] === str[str.length - 1]) {
+    return isPalindrome(str.substring(1, str.length - 1))
+  }
+
+  return false
+}
+
+function addUpTo() {
+
+}
